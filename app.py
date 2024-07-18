8fc90d5cafce88c61fd2d04ebd30572519bfe0b1
Enzo Marrama <enztek@gmail.com>
2024-07-17 17:12:09 -0700

    Added an explicit continue to odrive init loop

--- app.py
+++ app.py
@@ -135,11 +135,13 @@ def motor_control_consumer():
             drive = odrive.find_any()
             drive.axis0.requested_state = odrive.utils.AXIS_STATE_CLOSED_LOOP_CONTROL
             drive.axis1.requested_state = odrive.utils.AXIS_STATE_CLOSED_LOOP_CONTROL
+            drive.axis0.config.watchdog_timeout=3
+            drive.axis1.config.watchdog_timeout=3
             drive.axis0.config.enable_watchdog=True
             drive.axis1.config.enable_watchdog=True
-            drive.axis0.config.watchdog_timeout=1
-            drive.axis1.config.watchdog_timeout=1
-
+            odrive.axis0.watchdog_feed()
+            odrive.axis1.watchdog_feed()
+            
             odrive.utils.dump_errors(drive, clear=True)
             if (drive.error != 0) or (drive.axis0.error != 0) or (drive.axis1.error != 0):
                 drive.reboot()
